42dcd79183801e6d54664c58caa108860b7476c1
smturek <smturek@gmail.com>
2014-10-21 11:15:36 -0600

    Bootstrapified Tasks and added checkbox

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to @task, notice: 'Task was successfully created.'}
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:description)
+      params.require(:task).permit(:description, :complete)
     end
 end
