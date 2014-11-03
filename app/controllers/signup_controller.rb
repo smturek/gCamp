dbb6ec974cef28ae29265ea31efb13b93ae796ef
smturek <smturek@gmail.com>
2014-11-03 14:38:56 -0700

    added sign up/in links

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -13,7 +13,11 @@ class SignupController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name,
+                                :last_name,
+                                :email,
+                                :password,
+                                :password_confirmation)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,9 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email,
+                                  :password,:password_confirmation
+                                )
   end
 
   def set_user
