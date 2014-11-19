43c421fb3b8be5a6c11b04c6273c3981ca38e3aa
smturek <smturek@gmail.com>
2014-11-19 13:32:38 -0700

    fixed tests so they now pass

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 feature "Tasks" do
 
   scenario "User creates a task" do
-    visit tasks_path
+    project = Project.create!(name: "My Project")
+    visit project_path(project)
+    click_on "0 Tasks"
     expect(page).to have_no_content("Task 1")
     click_on "New Task"
     fill_in "Description", with: "Task 1"
@@ -12,8 +14,12 @@ feature "Tasks" do
   end
 
   scenario "User edits a task" do
-    Task.create(description: 'Task 1')
-    visit tasks_path
+    project = Project.create!(name: "My Project")
+    visit project_path(project)
+    click_on "0 Tasks"
+    click_on "New Task"
+    fill_in "Description", with: "Task 1"
+    click_on "Create Task"
     click_on "Edit"
     fill_in "Description", with: "Task 2"
     click_on "Update Task"
@@ -22,23 +28,33 @@ feature "Tasks" do
   end
 
   scenario "User destroys a task" do
-    Task.create(description: 'Task 1')
-    visit tasks_path
+    project = Project.create!(name: "My Project")
+    visit project_path(project)
+    click_on "0 Tasks"
+    click_on "New Task"
+    fill_in "Description", with: "Task 1"
+    click_on "Create Task"
     expect(page).to have_content("Task 1")
+    click_on "Back"
     click_on "Destroy"
     expect(page).to have_no_content("Task 1")
   end
 
   scenario "User attempts to create empty task" do
-    visit tasks_path
+    project = Project.create!(name: "My Project")
+    visit project_path(project)
+    click_on "0 Tasks"
     click_on "New Task"
     click_on "Create Task"
     expect(page).to have_content("Description can't be blank")
   end
 
   scenario "User attempts to create a task in the past" do
-    visit tasks_path
+    project = Project.create!(name: "My Project")
+    visit project_path(project)
+    click_on "0 Tasks"
     click_on "New Task"
+    fill_in "Description", with: "Task 1"
     fill_in "Due date", with: "1/1/1901"
     click_on "Create Task"
     expect(page).to have_content("Due date can't be in the past")
