63aa7efc646c65ade5bf4092a57e3a45f2bd2006
smturek <smturek@gmail.com>
2014-12-03 16:23:51 -0700

    made dropdown global

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action do
+    @projects = Project.all
+  end
 
   def current_user
     User.find_by(id: session[:user_id])
